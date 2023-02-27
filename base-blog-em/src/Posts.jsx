d4f8635301bc2ecd7b5c3531dfb91315d403fd1b
xtring <hh940630@gmail.com>
2023-02-27 21:58:42 +0900

    useQuery를 통해 비동기 요청을 하고 반환된 데이터를 사용하여 포스트 데이터를 보여줘라

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import { useQuery } from 'react-query';
+
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
@@ -14,8 +16,9 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const { data } = useQuery('posts', fetchPosts);
+  
+  if (!data) return <div />;
 
   return (
     <>
