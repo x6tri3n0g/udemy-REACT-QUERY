1e014b621a4c676ef9e67b89ee7ef1454525957e
xtring <hh940630@gmail.com>
2023-02-27 22:39:05 +0900

    isError, error, isLoading의 상태를 통해서 각 상황별 UI를 노출하라

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -16,9 +16,18 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data } = useQuery('posts', fetchPosts);
+  const { data, isError, error, isLoading } = useQuery('posts', fetchPosts);
   
-  if (!data) return <div />;
+  if (isLoading) return <h3>Loading...</h3>;
+
+  if (isError) {
+    return (
+      <>
+        <h3>Oops, something went wrong!</h3>
+        <p>{error.toString()}</p>
+      </>
+    )
+  }
 
   return (
     <>
