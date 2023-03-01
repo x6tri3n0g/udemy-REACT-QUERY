7e20339fa6677bcd755b94d5b7c8bc8147def3c4
xtring <hh940630@gmail.com>
2023-03-01 18:52:42 +0900

    currentPage의 상태를 통해 Pagination을 구현하라

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -5,9 +5,9 @@ import { useQuery } from 'react-query';
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
@@ -15,12 +15,14 @@ async function fetchPosts() {
 const POSTS_STALE_TIME = 2000;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data, isError, error, isLoading } = useQuery('posts', fetchPosts, { 
-    staleTime: POSTS_STALE_TIME,
-  });
+  const { data, isError, error, isLoading } = useQuery(
+    ['posts', currentPage],
+    () => fetchPosts(currentPage),
+    { staleTime: POSTS_STALE_TIME }
+  );
   
   if (isLoading) return <h3>Loading...</h3>;
 
@@ -47,11 +49,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button 
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button 
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue + 1);
+          }}
+        >
           Next page
         </button>
       </div>
