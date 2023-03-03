8b81dfc9cd85b9d75dffa7b61cebdb24950826ba
xtring <hh940630@gmail.com>
2023-03-03 23:49:27 +0900

    useMutation을 통해 deleting을 구현하고 mutate를 통해 주기를 컨트롤하라

--- base-blog-em/src/PostComments.jsx
+++ base-blog-em/src/PostComments.jsx
@@ -8,14 +8,6 @@ async function fetchComments(postId) {
   return response.json();
 }
 
-async function deletePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
-  );
-  return response.json();
-}
-
 async function updatePost(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/postId/${postId}`,
--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,10 +1,30 @@
 import PostComments from "./PostComments";
 
+import { useMutation } from 'react-query';
+
+async function deletePost(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "DELETE" }
+  );
+  return response.json();
+}
+
 export function PostDetail({ post }) {
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button
+        onClick={() => deleteMutation.mutate(post.id)}
+      >
+        Delete
+      </button>
+      {deleteMutation.isError && <p style={{ color: 'red' }}>Error deleting the post.</p>}
+      {deleteMutation.isLoading && <p style={{ color: 'purple' }}>Deleting the post.</p>}
+      {deleteMutation.isSuccess && <p style={{ color: 'green' }}>Post has (not) been deleted.</p>}
+      <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
       <PostComments postId={post.id} />
