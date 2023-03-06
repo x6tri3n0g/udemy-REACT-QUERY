1bea05ba1f94916d939db2d4c0319b726511c53a
xtring <hh940630@gmail.com>
2023-03-07 01:40:55 +0900

    useMutation을 통해 포스트의 타이틀을 업데이트하라

--- base-blog-em/src/PostComments.jsx
+++ base-blog-em/src/PostComments.jsx
@@ -1,17 +1,8 @@
-import { useMemo } from 'react';
 import { useQuery } from 'react-query';
 
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-  );
-  return response.json();
-}
-
-async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
   );
   return response.json();
 }
--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -10,8 +10,18 @@ async function deletePost(postId) {
   return response.json();
 }
 
+async function updatePost(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+  );
+  console.log(response)
+  return response.json();
+}
+
 export function PostDetail({ post }) {
   const deleteMutation = useMutation((postId) => deletePost(postId));
+  const udpateMutation = useMutation((postId) => updatePost(postId));
 
   return (
     <>
@@ -24,7 +34,14 @@ export function PostDetail({ post }) {
       {deleteMutation.isError && <p style={{ color: 'red' }}>Error deleting the post.</p>}
       {deleteMutation.isLoading && <p style={{ color: 'purple' }}>Deleting the post.</p>}
       {deleteMutation.isSuccess && <p style={{ color: 'green' }}>Post has (not) been deleted.</p>}
-      <button>Update title</button>
+      <button
+        onClick={() => udpateMutation.mutate(post.id)}
+      >
+        Update title
+      </button>
+      {udpateMutation.isError && <p style={{ color: 'red' }}>Error updating the post.</p>}
+      {udpateMutation.isLoading && <p style={{ color: 'purple' }}>Updating the post.</p>}
+      {udpateMutation.isSuccess && <p style={{ color: 'green' }}>Post has (not) been updated.</p>}
       <p>{post.body}</p>
       <h4>Comments</h4>
       <PostComments postId={post.id} />
